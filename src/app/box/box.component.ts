c794bfcdf07b6cc066df94297eaded71ec7145a0
su469 <sumareddy8476@gmail.com>
2021-05-25 23:01:38 +0530

    Initial commit

---
+++ src/app/box/box.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-box',
+  templateUrl: './box.component.html',
+  styleUrls: ['./box.component.css']
+})
+export class BoxComponent implements OnInit {
+  constructor() {}
+  @Input('values') abc;
+
+
+  ngOnInit() {}
+}
